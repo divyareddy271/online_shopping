d3342db210acd254b9f09a678f7798ac5787657b
Divya <divyareddy271999@gmail.com>
2022-03-09 16:10:34 +0530

    added firebase db with get and onsnapshot methods

--- Desktop/cart/src/App.js
+++ Desktop/cart/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CartItem from "./cartitem"
 import Cart from "./cart"
 import Navbar from "./navbar"
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 import { render } from '@testing-library/react';
 class App extends React.Component{
     constructor(){
@@ -9,7 +11,7 @@ class App extends React.Component{
       super();
       this.state = {
         products: [
-          {
+          /*{
             price: 20000,
             title: "Watch",
             qty: 1,
@@ -29,10 +31,55 @@ class App extends React.Component{
             qty: 6,
             img: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
             id: 2,
-          },
-        ]
+          },*/
+        ],
+        loading : true,
       }
+      this.db = firebase.firestore();
       //this.increasequantity = this.increasequantity.bind(this);
+    }
+    componentDidMount(){
+      /*firebase
+      .firestore()
+      .collection("products")
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot);
+        
+      const products = snapshot.docs.map((doc) => {
+        const data = doc.data(); 
+        console.log(doc.data());
+        data["id"]=doc.id;
+        return data;
+      })
+      snapshot.docs.map((doc) => {
+        console.log(doc.data());
+      })
+        this.setState ({
+          products,
+          loading : false
+        })
+    })*/
+    this.db
+      .collection("products")
+      .onSnapshot((snapshot) => {
+        console.log(snapshot);
+        
+      const products = snapshot.docs.map((doc) => {
+        const data = doc.data(); 
+        console.log(doc.data());
+        data["id"]=doc.id;
+        return data;
+      })
+      snapshot.docs.map((doc) => {
+        console.log(doc.data());
+      })
+        this.setState ({
+          products,
+          loading : false
+        })
+    })
+     
     }
     handleincreaseQuantity = (product) => {
       console.log("increase qunty", product);
@@ -85,7 +132,7 @@ class App extends React.Component{
       return Totalcount;
     }
     render(){
-      const {products,onIncreaseQuantity,ondecreaseQuantity} = this.state
+      const {products,onIncreaseQuantity,ondecreaseQuantity,loading} = this.state
       return (
         <div className="App">
           <Navbar 
@@ -98,11 +145,41 @@ class App extends React.Component{
            ondecreaseQuantity = {this.handledecreaseQuantity}
            Ondelete = {this.handledeleteproduct}
           />
+          {loading && <h1>Loading Products...</h1>}
           <div style={{padding : 10,fontSize:20}}>Total: {this.getCartTotal()}</div>
+          
         </div>
       );
     }
 
-  }
+  }/*
+  import React from "react";
 
+  class App extends React.Component{
+    constructor() {
+      super();
+      this.state = {
+        value: 0
+      };
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+      if (nextState.value > 5) return false;
+      return true;
+    }
+   
+    componentDidMount() {
+      setInterval(() => {
+        this.setState((prevState) => {
+          return {
+            value: prevState.value + 1
+          };
+        });
+      });
+    }
+    render() {
+      console.log(this.state.value);
+      return <div>{this.state.value}</div>;
+    }
+  }
+  */
   export default App;
--- Desktop/cart/src/index.js
+++ Desktop/cart/src/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANverbxH-93LOSm99qD9b5hm6k0-7Nvjk",
+  authDomain: "cart-baa01.firebaseapp.com",
+  projectId: "cart-baa01",
+  storageBucket: "cart-baa01.appspot.com",
+  messagingSenderId: "409734047661",
+  appId: "1:409734047661:web:e282aee04eff0774db2324"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <App />,
